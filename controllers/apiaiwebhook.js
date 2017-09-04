1913ceab4f69c7e6fbba3c64cf45a9b71e498ac6
Ken Yasue <ken.yasue@clover-studio.com>
2017-09-05 00:14:08 +0200

    added Yahoo API call

--- controllers/apiaiwebhook.js
+++ controllers/apiaiwebhook.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const YQL = require('yql');
 
 class ApiAIHandler {
 
@@ -7,26 +8,32 @@ class ApiAIHandler {
 
         router.post('/webhook', (req,res) => {
 
-            console.log('Received POST request.',JSON.stringify(req.body, null, 3));
 
+            console.log('Received POST request.',JSON.stringify(req.body, null, 3));
+            
             let city = req.body.result.parameters['geo-city'];
             let date = req.body.result.parameters['date'];
 
             if(date == '')
                 date = 'today';
-        
-            if(city && date){
+            
+            this.askWather(city).then((data) => {
+
+                console.log('Yahoo API response.',JSON.stringify(data, null, 3));
+
                 res.json({
                     'speech':" Weather for " + date + " in " + city + "? Probably it is sunny.",
                     'displayText':" Weather for " + date + " in " + city + "? Probably it is sunny.",
-                })
-            } else {
+                });
+
+            }).catch(() => {
+
                 res.json({
                     'speech':req.body.result.fulfillment.speech,
                     'displayText':req.body.result.fulfillment.speech
                 });
-            }
 
+            });
 
         });
 
@@ -34,6 +41,27 @@ class ApiAIHandler {
 
     }
 
-}
+    askWather(location){
+        
+        return new Promise((resolve,reject) => {
+            
+            const query = new YQL('select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="'+location+'")');
+            
+            query.exec((err, data) => {
+              var location = data.query.results.channel.location;
+              var condition = data.query.results.channel.item.condition;
+              
+              if(err){
+                  reject()
+              }else{
+                  resolve(data);
+              }
+
+            });
+
+        });
+        
+    }
+} 
 
 module["exports"] = ApiAIHandler;
