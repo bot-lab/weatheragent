706931a094e0c73ab987b920e8c957162d568439
Ken Yasue <ken.yasue@clover-studio.com>
2017-09-05 00:26:19 +0200

    workgin on yahoo api

--- controllers/apiaiwebhook.js
+++ controllers/apiaiwebhook.js
@@ -9,8 +9,6 @@ class ApiAIHandler {
         router.post('/webhook', (req,res) => {
 
 
-            console.log('Received POST request.',JSON.stringify(req.body, null, 3));
-            
             let city = req.body.result.parameters['geo-city'];
             let date = req.body.result.parameters['date'];
 
@@ -18,44 +16,58 @@ class ApiAIHandler {
                 date = new Date().toISOString().slice(0,10);
             }
             
-            this.askWather(city).then((data) => {
-
-                console.log('Yahoo API response.',JSON.stringify(data, null, 3));
-
-                const forecasts = data.query.results.channel.item.forecast;
-
-                let forecast = forecasts.find((item) => {
-
-                    const formatedDate = new Date(item.date).
-                    toLocaleString('en-us', {year: 'numeric', month: '2-digit', day: '2-digit'}).
-                    replace(/(\d+)\/(\d+)\/(\d+)/, '$3-$1-$2');
-                    
-                    return formatedDate = date;
-
+            if(city){
+                
+                this.askWather(city).then((data) => {
+    
+                    console.log('Yahoo API response.',JSON.stringify(data, null, 3));
+    
+                    const forecasts = data.query.results.channel.item.forecast;
+    
+                    let forecast = forecasts.find((item) => {
+    
+                        const formatedDate = new Date(item.date).
+                        toLocaleString('en-us', {year: 'numeric', month: '2-digit', day: '2-digit'}).
+                        replace(/(\d+)\/(\d+)\/(\d+)/, '$3-$1-$2');
+                        
+                        return formatedDate = date;
+    
+                    });
+    
+                    if(forecast){
+                        
+                        res.json({
+                            'speech':" Weather for " + date + " in " + city + "? It is " + forecast.text,
+                            'displayText':" Weather for " + date + " in " + city + "? It is " + forecast.text
+                        });
+    
+                    } else {
+    
+                        res.json({
+                            'speech':" Sorry Yahoo Weather API doesnt know the answer.",
+                            'displayText':" Sorry Yahoo Weather API doesnt know the answer."
+                        });
+    
+                    }
+    
+                }).catch(() => {
+    
+                    res.json({
+                        'speech':req.body.result.fulfillment.speech,
+                        'displayText':req.body.result.fulfillment.speech
+                    });
+    
                 });
 
-                if(forecast){
-                    
-                    res.json({
-                        'speech':" Weather for " + date + " in " + city + "? It is " + forecast.text,
-                        'displayText':" Weather for " + date + " in " + city + "? It is " + forecast.text
-                    });
-
-                } else {
-                    res.json({
-                        'speech':" Sorry Yahoo Weather API doesnt know the answer.",
-                        'displayText':" Sorry Yahoo Weather API doesnt know the answer."
-                    });
-                }
-
-            }).catch(() => {
+            } else {
 
                 res.json({
                     'speech':req.body.result.fulfillment.speech,
                     'displayText':req.body.result.fulfillment.speech
                 });
 
-            });
+            }
+            
 
         });
 
