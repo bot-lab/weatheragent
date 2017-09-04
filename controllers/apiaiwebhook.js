5f5dd089a81b7d86d5964bf145ebfc79d86f6587
Ken Yasue <ken.yasue@clover-studio.com>
2017-09-04 17:02:33 +0200

    debugging

--- controllers/apiaiwebhook.js
+++ controllers/apiaiwebhook.js
@@ -9,12 +9,12 @@ class ApiAIHandler {
 
             console.log('Received POST request.',req.body);
 
-            const city = req.body.result.parameters['geo-city'];
-            const date = req.body.result.parameters['date'];
+            let city = req.body.result.parameters['geo-city'];
+            let date = req.body.result.parameters['date'];
 
             if(date == '')
                 date = 'today';
-            
+
             res.json({
                 'speech':" Weather for " + date + " in " + city + "? Probably it is sunny.",
                 'displayText':" Weather for " + date + " in " + city + "? Probably it is sunny.",
