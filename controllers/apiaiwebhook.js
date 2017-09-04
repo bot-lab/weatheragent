0a2548572761c8d31adef5db3df0a2dce3c2417d
Ken Yasue <ken.yasue@clover-studio.com>
2017-09-04 14:24:57 +0200

    add default text

--- controllers/apiaiwebhook.js
+++ controllers/apiaiwebhook.js
@@ -7,7 +7,15 @@ class ApiAIHandler {
 
         router.post('/webhook', (req,res) => {
 
+            const city = req.body.parameters['geo-city'];
+            const date = req.body.parameters['date'];
+
             console.log('Received POST request.',req.body);
+            
+            res.json({
+                'speech':'Tomorrow is sunny day.',
+                'displayText':'Tomorrow is sunny day.'
+            })
 
         });
 
